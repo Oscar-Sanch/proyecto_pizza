10213e4ef958fc294918312e7e43bc090c2050d6
Oscar-Sanch <77586680+Oscar-Sanch@users.noreply.github.com>
2021-02-01 16:54:13 -0600

    Update proyecto pizza.py

--- proyecto pizza.py
+++ proyecto pizza.py
@@ -5,7 +5,6 @@ Created on Sun Jan 31 18:08:58 2021
 @author: pc
 """
 
-
 def desplegarMenu(texto,max):
     while (True):
         for i in texto:
@@ -14,11 +13,10 @@ def desplegarMenu(texto,max):
         if (opcion >0 and opcion <= max):
             return opcion
 
-def insertarOrden(orden,cliente):
+def insertarOrden(orden,cliente,ing,num):
     opcion=1
     total=[]
     textoMenu=("\n\n\t  Catálogo de Pizzas","\t**********************""\n\t1)Pepperoni","\t2)Hawuaiana","\t3)Especial","\t4)Mexicana","\t5)Ranchera","\t6)Completar orden","\nOpción: ")
-    ing=('Pepperoni','Cerezas','Piña','Jamón','Chorizo','Frijoles','Aguacate','Salchicha','Jalapeños','Tocino','Cebolla')
     listaOrden=[]
     while (opcion !=6):
         opcion= desplegarMenu(textoMenu,len(textoMenu)-2)
@@ -86,15 +84,14 @@ def insertarOrden(orden,cliente):
             precio=239.610
             listaOrden.append([clave,nombre,ingredientes,precio])
             total.append(precio)
-            listar1(listaOrden) 
-    nom=input("Nombre del Cliente: ")    
-    numero=1
-    print(f"Número de orden: {numero}")
+            listar1(listaOrden)
+    nom=input("Nombre del Cliente: ")
+    print(f"Número de orden: {num}")
     cantidad=len(listaOrden)
     print(f"Número de items: {cantidad}")
     print("Su orden es: ")
     listar1(listaOrden) 
-    cliente.append([numero,nom,cantidad,sum(total)])
+    cliente.append([num,nom,cantidad,sum(total)])
     sum(total)
     print(f"\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tSu total es: ${sum(total)}")
     orden.append(listaOrden)
@@ -119,25 +116,27 @@ def listar2(cliente):
         print()
         
 def buscarCliente(cliente):
-    nombre=input("\t  Nombre: ")
-    pos=[unCliente[1] for unCliente in cliente].index(nombre)
-    print(f"Orden de {nombre} servida!\n")
+    num=int(input("\t  # de Orden: "))
+    pos=[unCliente[0] for unCliente in cliente].index(num)
+    print(f"Orden de {cliente[pos][1]} servida!\n")
     return pos
-    
-        
+           
 def borrar(cliente):
     pos=buscarCliente(cliente)
     del(cliente[pos])
 
 def main():
+    num=0
     textoMenu=("\n\t\t   PIZZA DOMINO","\t=======================","\t\t 1)Ordenar","\t\t 2)Servir Orden","\t\t 3)Salir","Seleccione una opción:")      
+    ing=('Pepperoni','Cerezas','Piña','Jamón','Chorizo','Frijoles','Aguacate','Salchicha','Jalapeños','Tocino','Cebolla')
     cliente=[]
     orden = []
     opcion =1
     while (opcion !=3):
         opcion= desplegarMenu(textoMenu,len(textoMenu)-2)
         if(opcion==1):
-            insertarOrden(orden,cliente)
+            num+=1
+            insertarOrden(orden,cliente,ing,num)
             print("\n")
             print("\t\tOrdenes en espera:\n")
             listar2(cliente)
@@ -147,6 +146,6 @@ def main():
             borrar(cliente)
             print("\n\t\tOrdenes en espera:\n")
             listar2(cliente)
-            print("")   
-                           
+            print("")
+        
 main()
